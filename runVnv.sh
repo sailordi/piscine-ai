2140de02046034190087b708a3b258f681e74a26
sailordi <sailordi11@gmail.com>
2023-09-20 13:39:13 +0300

    numpy ex06 Done

--- runVnv.sh
+++ runVnv.sh
@@ -4,11 +4,16 @@ read -p "Enter the name for the virtual environment: " env_name
 read -p "Enter the notebook name (default is Notebook_${env_name}): " notebook_name
 notebook_name=${notebook_name:-"Notebook_${env_name}"}
 
+# Prompt the user for the port number
+read -p "Enter the port number (default is 8891): " port_number
+port_number="${port_number:-8891}"
+
 cd "$env_name"
 
-echo "{ \"cells\": [], \"metadata\": {}, \"nbformat\": 4, \"nbformat_minor\": 2 }" > "$notebook_name.ipynb"
-
 # Launch Jupyter Notebook on port 8891 with the specified notebook name
-jupyter notebook --port $port $notebook_name.ipynb
+    #jupyter notebook --ip=127.0.0.1 --port $port $notebook_name.ipynb
+
+# Launch Jupyter lab on port 8891 with the specified notebook name
+jupyter lab --ip=127.0.0.1 --port $port $notebook_name.ipynb
 
 cd ..

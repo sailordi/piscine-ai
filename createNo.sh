09483817f1e29a88cce6f3afb5f0297b05a81562
sailordi <sailordi11@gmail.com>
2023-10-13 10:24:13 +0300

    Updated scripts

--- createNo.sh
+++ createNo.sh
@@ -14,8 +14,13 @@ jupiter_create() {
 read -p "Enter the name for the virtual environment: " env_name
 
 # Input name for jupiter notbook
-read -p "Enter the notebook name (default is Notebook_${env_name}): " notebook_name
-notebook_name=${notebook_name:-"Notebook_${env_name}"}
+read -p "Enter the notebook name (name format will be Notebook_ your input): " notebook_name
+
+if [[ -z "$notebook_name" ]]; then
+    echo "Notebook name is empty. Exiting script."
+    exit 1
+
+notebook_name= "Notebook_"+notebook_name
 
 # Activate the virtual environment
 source "$env_name/bin/activate" 
--- createVnv.sh
+++ createVnv.sh
@@ -54,8 +54,14 @@ read -p "Enter the name for the virtual environment: " env_name
 read -p "Enter required libraries (comma-separated): " libraries
 
 # Input name for jupiter notbook
-read -p "Enter the notebook name (default is Notebook_${env_name}): " notebook_name
-notebook_name=${notebook_name:-"Notebook_${env_name}"}
+# Input name for jupiter notbook
+read -p "Enter the notebook name (name format will be Notebook_ your input ): " notebook_name
+
+if [[ -z "$notebook_name" ]]; then
+    echo "Notebook name is empty. Exiting script."
+    exit 1
+
+notebook_name= "Notebook_"+notebook_name
 
 # Prompt the user for the port number
 read -p "Enter the port number (default is 8891): " port_number
